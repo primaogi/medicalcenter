8863def2c3a6bc8dd35e3243cd19f65cd14af2a3
PRIMA GANTENG <primaogi256@gmail.com>
2024-01-20 08:37:53 +0700

    PII MEDICAL APP v1.6 (20 Januari 2024)

--- app/src/main/java/com/medicalcenter/DetailActivity.java
+++ app/src/main/java/com/medicalcenter/DetailActivity.java
@@ -3,7 +3,6 @@ package com.medicalcenter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -31,7 +30,6 @@ public class DetailActivity extends AppCompatActivity {
     Boolean isSuperuser = false;
     ValueEventListener eventListener;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
